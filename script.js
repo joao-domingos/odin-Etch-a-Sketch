e5719db416f01474b6bb49514f22f0b28f1e348b
joao-domingos <joaopedro.mdomingos@gmail.com>
2024-09-07 22:28:26 -0300

    basic html, made a container, and im trying to add a grid with flexbox using only dom

---
+++ script.js
@@ -0,0 +1,10 @@
+const cont = document.querySelector(".container");
+
+
+const div = document.createElement("div");
+div.textContent = "test";
+div.style.backgroundColor = "red";
+
+
+
+cont.appendChild(div);
