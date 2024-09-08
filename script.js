9892ebe261a1b84423b09fbf93e5efeaf944f016
joao-domingos <joaopedro.mdomingos@gmail.com>
2024-09-07 23:58:40 -0300

    finnaly i got a gui, i was lacking baecause i forgot flex wrap

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM fully loaded and parsed");
-  });
+// make the script run as soon as the page loads
+document.addEventListener("DOMContentLoaded", () => {
+    //just to test DOMCONTENTLOADER
+    //console.log("DOM fully loaded and parsed");
+
+    //select the class container from html file
+    const container = document.querySelector(".container");
+
+    //need to create a div for each element of a grid, 3x3, 16x16
+    //need to always create a new div before append
+    // 256 = 16x16
+    for (let i = 0; i < 256; i++) {
+        const createDiv = document.createElement("div");
+        createDiv.textContent = i;
+        container.appendChild(createDiv);
+    }
+
+
+    // after above, querySelectorAll() and forEach
+
+})
