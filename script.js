855355dc363a44fa4c6806b01e3644e508e823fa
joao-domingos <joaopedro.mdomingos@gmail.com>
2025-03-30 20:59:44 -0300

    logica finalizada do ex, agora falta fazer tweaks finais

--- script.js
+++ script.js
@@ -9,40 +9,18 @@ document.addEventListener("DOMContentLoaded", () => {
     //need to create a div for each element of a grid, 3x3, 16x16
     //need to always create a new div before append
     // 256 = 16x16
-    for (let i = 0; i < 256; i++) {
-        const createDiv = document.createElement("div");
-        createDiv.textContent = i;
-        container.appendChild(createDiv);
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const createDiv = document.createElement("div");
+            createDiv.textContent = i;
+            container.appendChild(createDiv);
+        }
     }
-
-
-    // after above, querySelectorAll() and forEach
-    const quadrados = document.querySelectorAll(".container div");
-
-    quadrados.forEach((quadrado) => {
-        quadrado.addEventListener(
-            "mouseover",
-            (event) => {
-                // highlight the mouseover target
-                event.target.style.backgroundColor = "orange";
-          
-                // reset the color after a short delay
-                setTimeout(() => {
-                    event.target.style.backgroundColor = "";
-                }, 500);
-            },
-            false
-        );
-    });
 
     //ao clicar no botao, pedir ao usuario input para o novo grid, depois remover o atual e gerar o novo, 
     const resetButton = document.querySelector(".reset");
-    const modal = document.querySelector("dialog");
     resetButton.addEventListener("click", function() {
-        const paraRemover = document.querySelectorAll(".container div");
-        paraRemover.forEach((div) => {
-            div.remove();
-        })
+        let paraRemover = document.querySelectorAll(".container div");
         let numeroQuadrados = prompt("qual a quantidade de quadrados por lado?");
         while (isNaN(numeroQuadrados)
             || numeroQuadrados < 0
@@ -50,7 +28,45 @@ document.addEventListener("DOMContentLoaded", () => {
         ) {
             numeroQuadrados = prompt("insira um numero valido");
         }
+        paraRemover.forEach((div) => {
+            div.remove();
+        })
+        for (let i = 0; i < numeroQuadrados; i++) {
+            for (let j = 0; j < numeroQuadrados; j++) {
+                const createDiv = document.createElement("div");
+                createDiv.textContent = i;
+                container.appendChild(createDiv);
+            }
+        }
+        let tamanhoQuadrados = document.querySelectorAll(".container div");
+        let auxPorcentagem = (100 / numeroQuadrados) + '%';
+        console.log(auxPorcentagem);
+        tamanhoQuadrados.forEach((quadrado) => {
+            quadrado.style.width = auxPorcentagem;
+        });
+        pintar();
     });
 
+    pintar();
 })
 
+function pintar() {
+       // after above, querySelectorAll() and forEach
+       const quadrados = document.querySelectorAll(".container div");
+
+       quadrados.forEach((quadrado) => {
+           quadrado.addEventListener(
+               "mouseover",
+               (event) => {
+                   // highlight the mouseover target
+                   event.target.style.backgroundColor = "orange";
+             
+                   // reset the color after a short delay
+                   setTimeout(() => {
+                       event.target.style.backgroundColor = "";
+                   }, 500);
+               },
+               false
+           );
+       });
+}
